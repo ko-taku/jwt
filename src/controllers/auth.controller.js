fbebbe38e8598aaee955d201ae4fa384b49a6b67
ko-taku <kosumgul@naver.com>
2025-05-14 10:50:09 +0900

    complete

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,12 +7,28 @@ module.exports = {
     const { userId, password } = req.body;
     /*
       Todo: verifyUser 함수를 통해 유저임을 확인합니다.
-        - 유저가 아닐 경우, 401 응답코드와 함께 message의 값으로 '등록되지 않은 유저입니다.'를 반환
-        - 유저일 경우, 
+        
+        
+    */
+
+    const isUser = verifyUser(userId, password);
+
+    if (!isUser) {
+      // - 유저가 아닐 경우, 401 응답코드와 함께 message의 값으로 '등록되지 않은 유저입니다.'를 반환
+      return res.status(401).json({ message: '등록되지 않은 유저입니다.' });
+    }
+
+    /*
+      - 유저일 경우, 
             토큰을 생성하여
             200 응답코드와 함께 token의 값으로 토큰을 반환
     */
-    return res.json('not implemented');
+
+    const token = jwt.sign({ userId: userId }, secretKey, {
+      expiresIn: '15m',
+    });
+
+    return res.status(200).json({ token });
   },
 
   me: async (req, res) => {
@@ -22,6 +38,14 @@ module.exports = {
         200 응답코드와 함께 user의 값으로 유저 정보를 반환합니다.
         - 반환해야 하는 데이터 양식은 테스트 코드를 통해 확인합니다.
     */
-    return res.json('not implemented');
+
+    const user = await findUserByUserId(userId);
+    return res.status(200).json({
+      message: '토큰이 유효합니다',
+      user: {
+        userId: user.userId,
+        email: user.email,
+      },
+    });
   },
 };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,13 +9,34 @@ module.exports = {
             verify하여 디코딩된 정보를 req.decoded에 저장합니다.
             이후 다음 프로세스를 위해 next()을 실행합니다.
       */
-      return res.json('not implemented');
+
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).json({ message: '토큰이 존재하지 않습니다.' });
+      }
+      const token = authHeader.startsWith('Bearer ')
+        ? authHeader.split(' ')[1]
+        : authHeader;
+      const decoded = jwt.verify(token, secretKey);
+      req.decoded = decoded;
+
+      next();
     } catch (err) {
       /*
         Todo: err.name에 따라 조건에 맞는 응답을 반환합니다. 
           - TokenExpiredError : 419 응답코드와 함께 "토큰이 만료되었습니다."를 반환
           - JsonWebTokenError : 401 응답코드와 함께 "유효하지 않은 토큰입니다."를 반환
       */
+
+      // - TokenExpiredError : 419 응답코드와 함께 "토큰이 만료되었습니다."를 반환
+      if (err.name === 'TokenExpiredError') {
+        return res.status(419).json({ message: '토큰이 만료되었습니다.' });
+      }
+
+      // - JsonWebTokenError : 401 응답코드와 함께 "유효하지 않은 토큰입니다."를 반환
+      if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
+      }
     }
   },
 };
